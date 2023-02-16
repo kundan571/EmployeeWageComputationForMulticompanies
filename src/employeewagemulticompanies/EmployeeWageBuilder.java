d568560d2ef13af9bd7ee79c1bff33e62d01f61c
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 12:57:54 +0530

    Added: Part Time Employee Wage Calculated

--- src/employeewagemulticompanies/EmployeeWageBuilder.java
+++ src/employeewagemulticompanies/EmployeeWageBuilder.java
@@ -1,22 +1,24 @@
 package employeewagemulticompanies;
 
 public class EmployeeWageBuilder {
-    static final int IS_FULL_TIME = 1;
-    static final int EMP_RATE_PER_HOUR = 20;
+    static int IS_FULL_TIME = 2;
+    static int IS_PART_TIME = 1;
+    static int EMP_RATE_PER_HOUR = 20;
     public int employeeCheck(){
-        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
+        int empCheck =  (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
     public int dailyWageOfEmployee(int empCheck){
         int empHrs = 0;
         int totalWage = 0;
-        //checking if full time or not
-        switch (employeeCheck()) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        }
+        else if (employeeCheck() == IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else {
+            empHrs = 0;
         }
         totalWage = empHrs * EMP_RATE_PER_HOUR;
         return totalWage;
