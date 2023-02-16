fd9d1e9240777c5b0150d164f1838fcff3909e47
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 13:08:17 +0530

    Added: calculated Wage For a Month

--- src/employeewagemulticompanies/EmployeeWageBuilder.java
+++ src/employeewagemulticompanies/EmployeeWageBuilder.java
@@ -4,35 +4,42 @@ public class EmployeeWageBuilder {
     static final int IS_FULL_TIME = 1;
     static final int EMP_RATE_PER_HOUR = 20;
     static final int IS_PART_TIME = 2;
+    static final int NO_OF_WORKING_DAYS = 20;
 
-    public int employeeCheck() {
-        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+    public static int employeeCheck() {
+        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
         return empCheck;
     }
 
-    public int dailyWageOfEmployee(int empCheck) {
+    public int dailyWageOfEmployee() {
         int empHrs = 0;
+        int totalHrs = 0;
         int totalWage = 0;
-        switch (employeeCheck()) {
-            //checking if part time or not
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            //checking if full time or not
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingDays < NO_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            switch (employeeCheck()) {
+                //checking if Part time or not
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                //checking if full time or not
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalHrs += empHrs;
         }
-        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        // calculate monthly wage
+        totalWage = totalHrs * EMP_RATE_PER_HOUR;
         return totalWage;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         EmployeeWageBuilder employee = new EmployeeWageBuilder();
-        int isPresent = employee.employeeCheck();
-        System.out.println("Employe Wage: " + employee.dailyWageOfEmployee(isPresent));
+        System.out.println("Monthly Employee Wage: " + employee.dailyWageOfEmployee());
     }
 }
