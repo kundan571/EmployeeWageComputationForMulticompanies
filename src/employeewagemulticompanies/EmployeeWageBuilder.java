706a49b4eda0207d99f4d87435a943ce884ea4e8
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 12:52:01 +0530

    Added: Employee Daily Wage Calculated

--- src/employeewagemulticompanies/EmployeeWageBuilder.java
+++ src/employeewagemulticompanies/EmployeeWageBuilder.java
@@ -1,18 +1,30 @@
 package employeewagemulticompanies;
 
 public class EmployeeWageBuilder {
-     static int IS_FULL_TIME = 1;
-    public static int employeeCheck(){
-        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-        if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is present");
-        }
-        else{
-            System.out.println("Employee is absent");
-        }
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_RATE_PER_HOUR = 20;
+    public int employeeCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
         return empCheck;
     }
+    public int dailyWageOfEmployee(int empCheck){
+        int empHrs = 0;
+        int totalWage = 0;
+        //checking if full time or not
+        switch (employeeCheck()) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        return totalWage;
+    }
     public static void main(String[] args) {
-        employeeCheck();
+        System.out.println("Welcome to Employee Wage Computation");
+        EmployeeWageBuilder employee = new EmployeeWageBuilder();
+        int isPresent = employee.employeeCheck();
+        System.out.println(employee.dailyWageOfEmployee(isPresent));
     }
 }
