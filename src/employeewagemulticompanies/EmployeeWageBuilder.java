b74c389b03974e46d0d2697489380ffcf0a1a4c5
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 12:47:31 +0530

    Added: Attendance check

--- src/employeewagemulticompanies/EmployeeWageBuilder.java
+++ src/employeewagemulticompanies/EmployeeWageBuilder.java
@@ -1,7 +1,18 @@
 package employeewagemulticompanies;
 
 public class EmployeeWageBuilder {
+     static int IS_FULL_TIME = 1;
+    public static int employeeCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
+        return empCheck;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation For Multi Companies");
+        employeeCheck();
     }
 }
