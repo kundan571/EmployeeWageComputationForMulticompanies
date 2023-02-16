7e9f3e4fd78e9f3ab57a976e1979ef40c2d19712
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 13:03:37 +0530

    Added: solving Using Switch Case

--- src/employeewagemulticompanies/EmployeeWageBuilder.java
+++ src/employeewagemulticompanies/EmployeeWageBuilder.java
@@ -1,32 +1,38 @@
 package employeewagemulticompanies;
 
 public class EmployeeWageBuilder {
-    static int IS_FULL_TIME = 2;
-    static int IS_PART_TIME = 1;
-    static int EMP_RATE_PER_HOUR = 20;
-    public int employeeCheck(){
-        int empCheck =  (int) Math.floor((Math.random() * 10) % 3);
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_RATE_PER_HOUR = 20;
+    static final int IS_PART_TIME = 2;
+
+    public int employeeCheck() {
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
-    public int dailyWageOfEmployee(int empCheck){
+
+    public int dailyWageOfEmployee(int empCheck) {
         int empHrs = 0;
         int totalWage = 0;
-        if (empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else if (employeeCheck() == IS_FULL_TIME){
-            empHrs = 8;
-        }
-        else {
-            empHrs = 0;
+        switch (employeeCheck()) {
+            //checking if part time or not
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            //checking if full time or not
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
         totalWage = empHrs * EMP_RATE_PER_HOUR;
         return totalWage;
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         EmployeeWageBuilder employee = new EmployeeWageBuilder();
         int isPresent = employee.employeeCheck();
-        System.out.println(employee.dailyWageOfEmployee(isPresent));
+        System.out.println("Employe Wage: " + employee.dailyWageOfEmployee(isPresent));
     }
 }
